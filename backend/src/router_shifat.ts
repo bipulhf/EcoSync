0285f0481a53a32f783eb20c3bae7a23282e9f84
Shaifur Rahaman <shaifurrahamanshifat71@gmail.com>
2024-03-24 17:13:36 +0600

    Added Basic Database Schemas for Authentication

---
+++ backend/src/router_shifat.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { prisma } from "./db";
+
+const router_shifat = Router();
+
+export default router_shifat;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,12 @@
 import express from "express";
-import router_bipul from "./router_bipul";
+
 import router_shifat from "./router_shifat";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-app.use("/api", router_bipul);
+ 
 app.use("/api", router_shifat);
 
 export default app;
