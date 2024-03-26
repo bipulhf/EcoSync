9f948fb9b9bb3f6ebd19fc3f4d4e507d1554b680
Shaifur Rahaman <shaifurrahamanshifat71@gmail.com>
2024-03-26 16:12:05 +0600

    Implemented Get All Users and Get A Specific User API

--- backend/src/helpers/mailTransporter.ts
+++ backend/src/helpers/mailTransporter.ts
@@ -1,13 +1,21 @@
-import nodemailer from 'nodemailer';
-import smtpTransport from 'nodemailer-smtp-transport';
- 
-export const mailTransporter = nodemailer.createTransport(smtpTransport({
-  host: 'smtp.mailersend.net', // SMTP host (e.g., 'smtp.example.com')
+import nodemailer from "nodemailer";
+import smtpTransport from "nodemailer-smtp-transport";
+import Mail from "nodemailer/lib/mailer";
+
+export const mailTransporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com", // SMTP host (e.g., 'smtp.example.com')
   port: 587, // SMTP port (e.g., 587 for TLS)
   secure: false, // Use TLS (true for 465, false for other ports)
   auth: {
-    user: 'MS_eBR9qp@trial-yzkq340k2nxgd796.mlsender.net', // SMTP username
-    pass: '4F0fnRgbFrLKZtop' // SMTP password
-  }
-}));
+    user: process.env.SMTP_EMAIL, // SMTP username
+    pass: process.env.SMTP_PASSWORD, // SMTP password
+  },
+});
 
+export const mailOptions: Mail.Options = {
+  from: `EcoSync <${process.env.SMTP_EMAIL}>`,
+  to: "",
+  subject: "",
+  html: "",
+};
--- backend/src/router_shifat.ts
+++ backend/src/router_shifat.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { prisma } from "./db";
-import { createUser, login,authenticateToken, resetPassword, resetPasswordConfirm, changePassword  } from "./auth";
+import { createUser, login,authenticateToken, resetPassword, resetPasswordConfirm, changePassword, getAllUsers, getUser  } from "./auth";
 
 const router_shifat = Router();
 
@@ -11,4 +11,7 @@ router_shifat.post('/authenticate', authenticateToken)
 router_shifat.post('/reset-password', resetPassword)
 router_shifat.post('/reset-password-confirm', resetPasswordConfirm)
 router_shifat.post('/change-password', changePassword)
+router_shifat.get('/user', getAllUsers)
+router_shifat.get('/user/:id', getUser)
+
 export default router_shifat;
